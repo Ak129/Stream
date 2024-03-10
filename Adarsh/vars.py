e4b162bd413b22aad59a267df727a0a3b72a9974
Ak129 <64674981+Ak129@users.noreply.github.com>
2024-03-10 15:56:11 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -17,7 +17,7 @@ class Var(object):
     WORKERS = int(getenv('WORKERS', '4'))
     BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002127693356'))
     PORT = int(getenv('PORT', 8080))
-    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', 'streaming-3136fd0577af.herokuapp.com'))
+    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', 'movieshubproo.apkthugs.com'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
     OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "5454532062").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
